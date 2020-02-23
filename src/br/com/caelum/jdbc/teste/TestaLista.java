31609845fc09c9892ec82d7a2fa9df5d98765889
Johnliveira <jonhjonholiveira@gmail.com>
2020-02-23 20:57:54 -0300

    insere e busca os contatos no banco de dados

---
+++ src/br/com/caelum/jdbc/teste/TestaLista.java
@@ -0,0 +1,25 @@
+package br.com.caelum.jdbc.teste;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import br.com.caelum.jdbc.dao.ContatoDao;
+import br.com.caelum.jdbc.modelo.Contato;
+
+public class TestaLista {
+	
+	public static void main(String[] args) {
+		
+		ContatoDao dao = new ContatoDao();
+		List<Contato> contatos = dao.getLista();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		for (Contato contato : contatos) {
+			System.out.println("Nome: " + contato.getNome());
+			System.out.println("Email: " + contato.getEmail());
+			System.out.println("Endereço: " + contato.getEndereco());
+			System.out.println("Data de Nascimento: " + sdf.format(contato.getDataNascimento().getTime()) + "\n");
+		}
+	}
+}
