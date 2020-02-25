318da576e11d1c8ee59f7ba895171703474466c9
Johnliveira <jonhjonholiveira@gmail.com>
2020-02-25 16:27:30 -0300

    Remove o Contato usando somente o id

---
+++ src/br/com/caelum/jdbc/teste/TestaRemove.java
@@ -0,0 +1,18 @@
+package br.com.caelum.jdbc.teste;
+
+import br.com.caelum.jdbc.dao.ContatoDao;
+import br.com.caelum.jdbc.modelo.Contato;
+
+public class TestaRemove {
+	
+	public static void main(String[] args) {
+		
+		ContatoDao dao = new ContatoDao();
+		Contato contato = new Contato();
+		
+		contato.setId((long) 3);
+		dao.remove(contato);
+		
+		System.out.println("Removido!");
+	}
+}
